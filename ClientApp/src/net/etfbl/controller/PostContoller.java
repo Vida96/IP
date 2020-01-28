22ee5ff76a8a895c8ee65622df812917ec654638
Nikola Vidovic <nikolavidovic47@yahoo.com>
2020-01-28 17:01:32 +0100

    Promjene u bazi oko duzine VARCHAR-ova oko objave. Popravljeno  slanja mail-a.

--- ClientApp/src/net/etfbl/controller/PostContoller.java
+++ ClientApp/src/net/etfbl/controller/PostContoller.java
@@ -102,10 +102,7 @@ public class PostContoller extends HttpServlet {
 		Integer isEmergency = jsonObject.getBoolean("isEmergency")? 1 : 0;
 		User postCreator = userBean.getUser();
 		
-		java.util.Date postTime = new java.util.Date();
-		java.text.SimpleDateFormat sdf = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		String currentTime = sdf.format(postTime);
-		
+		java.util.Date postTime = new java.util.Date();	
 		Post post = new Post(description, postCreator, postTime, location, video, isEmergency, link);
 		post.setImages(images);
 		post.setUserId(userBean.getUser().getId());
@@ -116,37 +113,28 @@ public class PostContoller extends HttpServlet {
 		{
 			sendMailToUsers(post, userBean.getUser().getMail());
 		}
-		
 	}
 
 	private void sendMailToUsers(Post post, String senderMail) {
 		List<String> usersMails = UserDAO.getUsersMailsForEmergencyMail(senderMail);
-sendMail("", "", post);
-	 for(String recieverMail : usersMails) {
-		System.out.println(recieverMail);	
+		sendMail("", "", post);
+		for(String recieverMail : usersMails) {
+		//	sendMail("", "", post);
+			System.out.println(recieverMail);	
 		} 
 	}
 
 	private void sendMail(String senderMail, String recieverMail, Post post) {
-		  // Recipient's email ID needs to be mentioned.
-        String to = "nikolavidovic813@gmail.com";
-
-        // Sender's email ID needs to be mentioned
+		String to = "nikolavidovic813@gmail.com";
         String from = "nikolavidovic813@gmail.com";
-
-        // Assuming you are sending email from through gmails smtp
         String host = "smtp.gmail.com";
 
-        // Get system properties
         Properties properties = System.getProperties();
-
-        // Setup mail server
         properties.put("mail.smtp.host", host);
         properties.put("mail.smtp.port", "465");
         properties.put("mail.smtp.ssl.enable", "true");
         properties.put("mail.smtp.auth", "true");
 
-        // Get the Session object.// and pass username and password
         Session session = Session.getInstance(properties, new javax.mail.Authenticator() {
 
             protected PasswordAuthentication getPasswordAuthentication() {
@@ -156,82 +144,15 @@ sendMail("", "", post);
             }
 
         });
-
-        // Used to debug SMTP issues
-//        session.setDebug(true);
-
+        
         try {
-       /* 	Message msg = new MimeMessage(session);
-        	 
-          
-        	// Create a default MimeMessage object.
             Message message = new MimeMessage(session);
-
-            // Set From: header field of the header.
             message.setFrom(new InternetAddress(from));
-
-            // Set To: header field of the header.
             message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-
-            // Set Subject: header field
-            message.setSubject("This is the Subject Line!");
-*/
-            // Now set the actual message
-         //   message.setText("This is actual message");
-        //    String msg2 = getMessageContent(post); // "<i>Greetings!</i><br>";
-          //  msg2 += "<b>Wish you a nice day!</b><br>";
-           // msg2 += "<font color=red>Duke</font><br>";
-          //  msg2 +="<img src='https://www.pngitem.com/pimgs/m/247-2473457_current-location-icon-png-location-vector-icon-transparent.png' alt='Smiley face' height='100' width='100'>";
-           // message.setContent(msg2, "text/html");
+            message.setSubject("Obavjestenje o hitnom upozorenju");
             
-            
-        	Message message = new MimeMessage(session);
-
-            // Set From: header field of the header.
-            message.setFrom(new InternetAddress(from));
-
-            // Set To: header field of the header.
-            message.setRecipients(Message.RecipientType.TO,
-               InternetAddress.parse(to));
-
-            // Set Subject: header field
-            message.setSubject("Testing Subject");
-
-            // This mail has 2 part, the BODY and the embedded image
-            MimeMultipart multipart = new MimeMultipart("related");
-
-            // first part (the html)
-            BodyPart messageBodyPart = new MimeBodyPart();
-            String htmlText = "<H1>Hello</H1><img src=\"cid:image\">";
-            messageBodyPart.setContent(htmlText, "text/html");
-            // add it
-            multipart.addBodyPart(messageBodyPart);
-
-            // second part (the image)
-            messageBodyPart = new MimeBodyPart();
-            DataSource fds = new FileDataSource(
-               "C:/Users/Nikola/Desktop/download.jfif");
-
-            messageBodyPart.setDataHandler(new DataHandler(fds));
-            messageBodyPart.setHeader("Content-ID", "<image>");
-            // add image to the multipart
-            multipart.addBodyPart(messageBodyPart);
-
-            messageBodyPart = new MimeBodyPart();
-            fds = new FileDataSource("C:/Users/Nikola/Desktop/images.jfif");
-
-            messageBodyPart.setDataHandler(new DataHandler(fds));
-            messageBodyPart.setHeader("Content-ID", "<image>");
-            // add image to the multipart
-            multipart.addBodyPart(messageBodyPart);
-
-            
-         
-            // put everything together
-      
-            message.setContent(multipart);
-                 System.out.println("sending...");
-            // Send message
+            String msg = getMessageContent(post);
+            message.setContent(msg, "text/html");
             Transport.send(message);
             System.out.println("Sent message successfully....");
         } catch (MessagingException mex) {
@@ -241,48 +162,35 @@ sendMail("", "", post);
 	
 	public String getMessageContent(Post post) {
 			String imagesHtml[] = {""};
-		    String categoriesHtml = "";
 		    String locationHtml = "";
 		    String descriptionHtml = "";
 		    Integer i = 0; 
 		    String location;
 		    String description;
 		    String link;
-		    
-		    /* categoriesNames.forEach(function (category, index) {
-		    	
-				if(categoriesNames.indexOf(category) == 0)
-					categoriesHtml+= "<h3 class='card-title'>Kategorije opasnosti: " + category;
-				else
-					categoriesHtml+= ", " + category;
-				
-				if (categoriesNames.indexOf(category) == (categoriesNames.length - 1))
-					categoriesHtml+=" </h3>";  
-		    }); */
-		   
+		 
 		    List<String> images = post.getImages(); 
 		    images.forEach(image -> {
-		//    	if(images.indexOf(image) == 0  && images.indexOf(image) == 4)
-			 imagesHtml[0] +="<img src=" + image +" alt='Smiley face' height='100' width='100'>";
-				
-		    	imagesHtml[0] +="<img src=" + image + "height='100' width='100'>";
-		 //       else
-		   // 		 imagesHtml[0] +="<img src='https://www.pngitem.com/pimgs/m/247-2473457_current-location-icon-png-location-vector-icon-transparent.png' alt='Smiley face' height='100' width='100'>";
-		     System.out.println(image);      
+		    	if(images.indexOf(image) == 0  && images.indexOf(image) == 4)
+		    		imagesHtml[0] +="<img src=" + image +" alt='Smiley face' height='100' width='100'>";
+		        else
+		      		imagesHtml[0] +="<img src=" + image +" alt='Smiley face' height='100' width='100'>";
 		    	}); 
 		    if(imagesHtml[0] != ""){
-		    	imagesHtml[0] += "<br><br><br><br><br><br><br><br><br>";
+		    	imagesHtml[0] += "<br><br><br>";
 		    }
 		    String linkHtml = "";
 
-		    if((location = post.getLocation()) != null && location == ""){
-		    	locationHtml+= "<i style='float:right' class='fa fa-map-marker' aria-hidden='true'>&nbsp;" + location + "</i>";
+		    if((location = post.getLocation()) != null && location != ""){
+		    	locationHtml+= "<p>Lokacija: " + location + "</p>";
 		    }
+		    
 		    if((description = post.getText()) != null){
-		    	descriptionHtml+="<p>" + description + "<br>";
+		    	descriptionHtml+="<p>Opis: " + description + "</p><br>";
 		    }
+		
 		    if((link = post.getLink()) != null){
-		    	linkHtml+="<a href=" + link + "class='card-link'>Pro&#269;itajte vi&#353;e</a>";
+		    	linkHtml+="<a href=" + link + ">Pro&#269;itajte vi&#353;e</a>";
 		    }
 		    
 		    return imagesHtml[0] + locationHtml + descriptionHtml + linkHtml;
